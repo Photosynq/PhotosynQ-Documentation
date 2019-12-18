9915d67412fe4e2319c1fe3527df894c31c30b83
Sebastian <sebastian.kuhlgert@gmail.com>
2019-12-18 15:05:31 -0500

    fix date in tag task

--- tasks/tag.js
+++ tasks/tag.js
@@ -4,17 +4,24 @@
 
 const {spawnSync} = require('child_process');
 
-const tag = function(cb){
-    // Get current tag from local repo
-    var tag = spawnSync('git', ['describe', '--abbrev=0', '--tags']);
-    tag = String(tag.stdout).trim();
+const getGitTag = function(){
+    // Get current tag name from current repo
+    var tag_name = spawnSync('git', ['describe', '--abbrev=0', '--tags']);
 
     // Get current tag date from current repo
     var tag_date = spawnSync('git', ['log', '--tags', '--simplify-by-decoration', '--pretty="%aI"', '--max-count=1']);
-    tag_date = String(tag_date.stdout).trim();
-    return new Promise(function(resolve, reject){
-        resolve({tag: tag, data: tag_date});
-    });
+    
+    return {
+        name: tag_name = tag_name.stdout.toString().trim(),
+        date: tag_date.stdout.toString().trim().slice(1,-1)
+    };
 };
 
-module.exports = tag;
+const showTag = function(cb){
+    // Get current tag from local repo
+    var tag = getGitTag();
+    console.log( `Name: ${tag.name}\nName: ${tag.date}\n` );
+    cb();
+};
+
+module.exports = { showTag, getGitTag };
