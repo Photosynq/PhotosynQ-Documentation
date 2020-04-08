cb9e6ec62677b8756f402a25e1fd3423b788f955
Sebastian <sebastian.kuhlgert@gmail.com>
2020-04-08 12:28:40 -0400

    remove unnecessary line breaks

--- tasks/instruments.js
+++ tasks/instruments.js
@@ -20,19 +20,19 @@ var instruments = function(cb){
                 var tab = `\n::: tab ${content.match(/^(# Settings -\s?)(.*)/m)[2]}\n`;
                 content = content.replace(/^(# Settings -)/gm,'#');
                 content = content.replace(/^(#+)/gm,'$1##');
-                options["instrument-settings"] += `${tab}\n${content}\n:::\n\n`;
+                options["instrument-settings"] += `${tab}\n${content}\n:::\n`;
             }
             if(info.name && info.name.match(/^calibrations/)){
                 var tab = `\n::: tab ${content.match(/^(# Calibrations -\s?)(.*)/m)[2]}\n`;
                 content = content.replace(/^(# Calibrations -)/gm,'#');
                 content = content.replace(/^(#+)/gm,'$1##');
-                options["instrument-calibrations"] += `${tab}\n${content}\n:::\n\n`;
+                options["instrument-calibrations"] += `${tab}\n${content}\n:::\n`;
             }
         }
         for(var out in options){
             if(options[out] == "")
                 continue;
-            options[out] = `:::: tabs type:card\n${options[out]}\n::::\n`;
+            options[out] = `:::: tabs type:card\n${options[out]}\n::::`;
             var tempPath = jetpack.path('instruments', 'docs', `${out}.md`);
             var template = jetpack.read(tempPath);
             var md = Mustache.render(template, options);
