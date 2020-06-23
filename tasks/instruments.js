d661e5c49c25a3095967084b1586ae1bcca08339
Sebastian <sebastian.kuhlgert@gmail.com>
2020-06-22 21:32:03 -0400

    correct links for assistants

--- tasks/instruments.js
+++ tasks/instruments.js
@@ -25,6 +25,9 @@ var instruments = function(cb){
             var tab = `\n::: tab ${content.match(/^(# Settings -\s?)(.*)/m)[2]}\n`;
             content = content.replace(/^(# Settings -)/gm,'#');
             content = content.replace(/^(#+)/gm,'$1##');
+            content = content.replace(/!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/g, function (match, g1, g2, g3) {
+                return `![${g1}](${g2.substr(1)})`;
+            });
             options["instrument-settings"] += `${tab}\n${content}\n:::\n`;
         }
 
@@ -35,6 +38,9 @@ var instruments = function(cb){
             content = jetpack.read(files[f]);
             content = content.replace(/^#\s{0,}(Calibrations|Assistants)(.*)\n\n/gm,'');
             content = content.replace(/^(#+)/gm,'$1#');
+            content = content.replace(/!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/g, function (match, g1, g2, g3) {
+                return `![${g1}](${g2.substr(1)})`;
+            });
             basename = file.name.slice(0,-3).replace(/(calibrations-|assistants-)/g,'');
             var type = file.name.slice(0,-3).split('-')[0];
             if(options[basename] !== undefined && options[basename] == "" )
@@ -48,15 +54,40 @@ var instruments = function(cb){
             var template = jetpack.read(tempPath);
             var docPath = "";
             var md = "";
-            if(out.match(/settings/)){
+            if(out.match(/settings/)){             
                 options[out] = `:::: tabs type:card\n${options[out]}\n::::`;
                 docPath = jetpack.path('docs', 'instruments', `${out}.md`);
                 md = Mustache.render(template, options);
+                var imgs = options[out].match(/\!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/gm);
+                if(imgs){
+                    imgs = imgs.map(function(x){
+                        var img = x.match(/\!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/)[2];
+                        var p = jetpack.path(filePath, img);
+                        if(jetpack.exists(p))
+                            jetpack.copy(p, jetpack.path('docs', 'instruments', img), { overwrite: true } );
+                        else
+                            console.log(`Error: ${p}`);
+                    });
+                }                   
             }
             else{
                 docPath = jetpack.path('docs', 'calibrations', `${out}.md`);
                 md = Mustache.render(template, options[out] );
-                console.log(Object.keys(options[out]))
+                Object.keys(options[out]).forEach(function(l){
+                    var imgs = options[out][l].match(/\!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/gm);
+                    if(imgs){
+                        imgs = imgs.map(function(x){
+                            var img = x.match(/\!\[([^\]]*)\]\((.*?)\s*("(?:.*[^"])")?\s*\)/)[2];
+                            var p = jetpack.path(filePath, img);
+                            if(jetpack.exists(p)){
+                                console.log( jetpack.path('docs', 'calibrations',  img) );
+                                jetpack.copy(p, jetpack.path('docs', 'calibrations',  img), { overwrite: true } );
+                            }
+                            else
+                                console.log(`Error: ${p}`);
+                        });
+                    }   
+                });
             }
             jetpack.write(docPath, md);
         }
