a84a864b61a696d3746ae5b3fdd5183038f9e9ba
Sebastian <sebastian.kuhlgert@gmail.com>
2020-08-24 16:39:25 -0400

    customize 404 page

---
+++ docs/.vuepress/theme/layouts/404.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="theme-container">
+    <div class="theme-default-content">
+      <h1>404</h1>
+      
+      <blockquote>{{ getMsg() }}</blockquote>
+
+      <RouterLink to="/">
+        Take me home.
+      </RouterLink>
+
+      <img 
+        style="color: #000;"
+        :src="$withBase('/404.png')"
+        alt="404 error"
+      >
+
+    </div>
+  </div>
+</template>
+
+<script>
+const msgs = [
+  `There's nothing here.`,
+  `How did we get here?`,
+  `That's a Four-Oh-Four.`,
+  `Looks like we've got some broken links.`
+]
+
+export default {
+  methods: {
+    getMsg () {
+      return msgs[Math.floor(Math.random() * msgs.length)]
+    }
+  }
+}
+</script>
